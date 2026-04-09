36a9551f030ceeb5b716970e73eec3f8f127ed90
Sarvesh <sarveswararao147@gmail.com>
2026-04-09 12:10:46 +0530

    initial commit

---
+++ datatypes_numeric.py
@@ -0,0 +1,35 @@
+a=10
+print(type(a)) #int
+
+b=10.2
+print(b)
+print(type(b)) #float
+
+c=1+2j
+print(c)
+print(type(c)) #complex
+
+d=True
+print(d)
+print(type(d)) #boolean
+
+e=False
+print(e)
+print(type(e)) #boolean
+
+f=None
+print(f)
+print(type(f)) #NoneType
+
+g=1+2j
+print(g)
+print(type(g)) #complex
+
+h=1+2j
+print(h)
+print(type(h)) #complex
+
+a=0.5 #float
+b=0.6
+print(a+b) #float
+print(type(a+b)) #float
